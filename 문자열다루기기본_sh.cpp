c8f79fe92d09882ce5d8ae08832264e590905d53
catsaveearth <kih629@gachan.ac.kr>
2022-06-21 23:03:21 +0900

    1day_sh

---
+++ 문자열다루기기본_sh.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    int len = s.length();
+
+    if (len == 4 || len == 6) {
+        for (char const& c : s) {
+            if (48 > c || c > 57) {
+                answer = false;
+                break;
+            }
+        }
+    }
+    else {
+        answer = false;
+    }
+    return answer;
+}
