f8584e5412ae77241793ae1820ded3506687f322
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-12 10:29:33 +0900

    level1_8_mj_11

---
+++ Level1/없는숫자더하기_mj.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int[] numbers)
+    {
+        int answer = 45;
+        foreach (int num in numbers)
+            answer -= num;
+        return answer;
+    }
+}
