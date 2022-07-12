524899a9369222e59f8010d25db949d842f7726a
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-12 09:54:57 +0900

    level1_6_mj_9

---
+++ Level1/내적_mj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int[] a, int[] b)
+    {
+        int answer = 0;
+        int i = 0;
+        foreach (int aNum in a)
+        {
+            answer += aNum * b[i];
+            i++;
+        }
+        return answer;
+    }
+}
