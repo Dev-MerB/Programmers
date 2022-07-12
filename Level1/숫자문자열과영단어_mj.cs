6b4c588ee996f2c8e64aa565b2541231f712830a
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-12 10:27:41 +0900

    level1_7_mj_10

---
+++ Level1/숫자문자열과영단어_mj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(string s)
+    {
+        int answer = 0;
+        string[] nums = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        for (int i = 0; i < 10; i++)
+            s = s.Replace(words[i], nums[i]);
+        answer = Int32.Parse(s);
+        return answer;
+    }
+}
