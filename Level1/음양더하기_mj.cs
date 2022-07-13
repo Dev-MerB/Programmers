73f4f07ed67a85dda7350b33ab0c1eec4703461f
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-13 15:02:21 +0900

    level1_9_mj_12

---
+++ Level1/음양더하기_mj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int[] absolutes, bool[] signs)
+    {
+        int answer = 0;
+        for (int i = 0; i < absolutes.Length; i++)
+            if (signs[i])
+                answer += absolutes[i];
+            else
+                answer -= absolutes[i];
+
+        return answer;
+    }
+}
