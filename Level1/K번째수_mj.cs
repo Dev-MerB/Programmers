068c58b1689c896cb315feb6151f06cc0c5b9833
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-13 15:42:44 +0900

    level1_10_mj_13

---
+++ Level1/K번째수_mj.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(int[] array, int[,] commands)
+    {
+        int[] answer = new int[commands.GetLength(0)];
+        for (int i = 0; i < commands.GetLength(0); i++)
+        {
+            int[] newArray = new int[commands[i, 1] - commands[i, 0] + 1];
+            Array.Copy(array, commands[i, 0] - 1, newArray, 0, commands[i, 1] - commands[i, 0] + 1);
+            Array.Sort(newArray);
+            answer[i] = newArray[commands[i, 2] - 1];
+        }
+        return answer;
+    }
+}
