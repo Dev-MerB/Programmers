98454e00554bf46b3ed90815247190d3266bfc2b
Jintopia <matrosica0808@gmail.com>
2022-07-14 10:54:35 +0900

    solved 17 problems by hi

---
+++ Level1/K번째수_hi.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for(auto cmd : commands)
+    {
+        vector<int> temp;
+        std::copy(array.begin()+cmd[0]-1,array.begin()+cmd[1],back_inserter(temp));
+        sort(temp.begin(), temp.end());
+        answer.push_back(temp[cmd[2]-1]);
+    }
+    return answer;
+}
