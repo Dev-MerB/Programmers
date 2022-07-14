7c18428d3fc18f238fb3c1793096c8513b91bc61
Jintopia <matrosica0808@gmail.com>
2022-07-14 11:14:53 +0900

    solved by 18 problems by hi

---
+++ Level1/약수의개수와덧셈_hi.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int getCount(int n)
+{
+    int cnt =0;
+    for(int i =1; i<=sqrt(n); i++)
+        if(n%i == 0) 
+            if(n/i==i)cnt++;
+            else cnt+=2;
+    return cnt;
+}
+
+int solution(int left, int right) {
+    int answer = 0;
+    for (int i= left;i<=right; i++)
+        if(getCount(i)%2) answer-=i;
+        else answer+= i;
+    return answer;
+}
+
