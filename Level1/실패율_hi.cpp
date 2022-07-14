b8a957b7bb8a6029ad1008f4d7012ef8b25f8704
Jintopia <matrosica0808@gmail.com>
2022-07-14 10:54:21 +0900

    solved 15 problems by hi

---
+++ Level1/실패율_hi.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(int N, vector<int> stages) {
+    vector<int> answer;
+    vector<double> rates;
+    double user = stages.size();
+    for(int i = 1; i<=N; i++)
+    {
+        double clear = 0;
+        for(auto stage: stages) if(stage == i) clear++;
+        if(clear == 0) rates.push_back(0);
+        else rates.push_back(clear/user);
+        user -= clear;
+    }
+    
+    for(int i=0;i<N; i++)
+    {
+        auto itr = max_element(rates.begin(),rates.end());
+        answer.push_back(itr-rates.begin()+1);
+        *itr = -1;
+    }
+    return answer;
+}
