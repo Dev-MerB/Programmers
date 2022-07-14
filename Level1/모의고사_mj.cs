2b17fbd6863fcacda572303aa1667b43e8df67f5
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-14 17:28:30 +0900

    level1_11_mj_14

--- Level1/모의고사_mj.cs
+++ Level1/모의고사_mj.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Solution
 {
-    public int solution(string s)
+    public int[] solution(int[] answers)
     {
-        int answer = 0;
-        string[] nums = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-        string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        List<int> answerList = new List<int>();
+        int[,] supo = { { 1, 2, 3, 4, 5, 0, 0, 0, 0, 0 }, { 2, 1, 2, 3, 2, 4, 2, 5, 0, 0 }, { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } };
+        int[] num = { 0, 0, 0 };
+        for (int i = 0; i < answers.Length; i++)
+        {
+            if (supo[0, i % 5] == answers[i])
+                num[0]++;
+            if (supo[1, i % 8] == answers[i])
+                num[1]++;
+            if (supo[2, i % 10] == answers[i])
+                num[2]++;
+        }
 
-        for (int i = 0; i < 10; i++)
-            s = s.Replace(words[i], nums[i]);
-        answer = Int32.Parse(s);
+        if (num[0] >= num[1] && num[0] >= num[2])
+            answerList.Add(1);
+        if (num[1] >= num[0] && num[1] >= num[2])
+            answerList.Add(2);
+        if (num[2] >= num[0] && num[2] >= num[1])
+            answerList.Add(3);
+
+        int[] answer = answerList.ToArray();
         return answer;
     }
 }
