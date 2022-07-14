1a6c5cc0ecbcc5366ffd3003676e4c64da54f0d7
Jintopia <matrosica0808@gmail.com>
2022-07-14 11:14:59 +0900

    solved by 19 problems by hi

---
+++ Level1/폰켓몬_hi.cpp
@@ -0,0 +1,13 @@
+﻿#include <vector>
+#include <set>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    set<int> s;
+    for(auto x: nums) s.insert(x);
+    if(s.size() <= nums.size()/2) answer = s.size();
+    else answer = nums.size()/2;
+    return answer;
+}
