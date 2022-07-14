9d89859f030b45cff00e722f2c3229c45e276d68
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-14 17:29:38 +0900

    level1_12_mj_15

---
+++ Level1/약수의개수와덧셈_mj.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int left, int right)
+    {
+        int answer = 0;
+        for (int num = left; num < right + 1; num++)
+        {
+            bool tf = true;
+            for (int i = 1; i < 32; i++)
+            {
+                if (num / i == i && num % i == 0)
+                    tf = false;
+            }
+            if (tf)
+                answer += num;
+            else
+                answer -= num;
+        }
+        return answer;
+    }
+}
