3ad1547e932e2cd89867c0f63caf71ee21bc0863
Jintopia <matrosica0808@gmail.com>
2022-07-14 10:54:29 +0900

    solved 16 problems by hi

---
+++ Level1/체육복_hi.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    vector<int> student(n+2,0);
+    for(auto idx: reserve) student[idx] +=1;
+    for(auto idx: lost) student[idx] -=1;
+    
+    for(int i = 1; i<=n; i++)
+    {
+        if(student[i] == -1)
+        {
+            if(student[i-1]==1)
+                student[i-1]= student[i] = 0;
+            else if(student[i+1] ==1)
+                student[i] = student[i+1] = 0;
+        }
+    }
+    
+    for(int i=1; i<=n;i++)
+        if(student[i]!=-1) answer++;
+    return answer;
+}
