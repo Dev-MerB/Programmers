dc262b7cb0efb6883790fbe761bb8e74b1b13c29
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-15 17:16:50 +0900

    level2_4_mj_18

---
+++ Level2/이진변환반복하기_mj.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(string s)
+    {
+        int iteration = 0;
+        int sum = 0;
+        int remove_cnt = 0;
+        while (s.Length != 1)
+        {
+            sum = 0;
+            char[] temp = s.ToCharArray();
+            foreach (char zeroOne in temp)
+            {
+                if (zeroOne == '0')
+                    sum++;
+            }
+            s = Convert.ToString(s.Length - sum, 2);
+            iteration++;
+            remove_cnt += sum;
+        }
+        int[] answer = { iteration, remove_cnt };
+        return answer;
+    }
+}
