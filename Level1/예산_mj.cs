7abde4e089fe6ac0ca9468c7a9d98c5d58d5d0ec
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-16 21:08:02 +0900

    level1_16_mj_20

---
+++ Level1/예산_mj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int[] d, int budget)
+    {
+        int answer = 0;
+        int sum = 0;
+        Array.Sort(d);
+        for (answer = 0; answer < d.Length; answer++)
+        {
+            sum += d[answer];
+            if (sum == budget)
+                return answer + 1;
+            else if (sum > budget)
+                return answer;
+        }
+        return answer;
+    }
+}
