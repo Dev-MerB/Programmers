be0804865cad560d7ab86336c2dbdfddadf1468a
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-16 21:07:40 +0900

    level1_15_mj_19

---
+++ Level1/3진법뒤집기_mj.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        List<int> arrayInt = new List<int>();
+        while (n > 0)
+        {
+            arrayInt.Add(n % 3);
+            n = n / 3;
+        }
+        answer = arrayInt[0];
+        for (int i = 1; i < arrayInt.Count; i++)
+            answer = answer * 3 + arrayInt[i];
+        return answer;
+    }
+}
