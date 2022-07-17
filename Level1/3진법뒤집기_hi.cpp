dc58ba1cfa2edabe0a6483ec99b6a0273e78016b
Jintopia <matrosica0808@gmail.com>
2022-07-17 23:23:03 +0900

    solved 20 problems by hi

---
+++ Level1/3진법뒤집기_hi.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v;
+    while(n)
+    {
+        v.push_back(n%3);
+        n/=3;
+    }
+    
+    int loc = 1;
+    for(int i =v.size()-1; i>=0; i--)
+    {
+        answer+= v[i]*loc;
+        loc *=3;
+    }
+    return answer;
+}
