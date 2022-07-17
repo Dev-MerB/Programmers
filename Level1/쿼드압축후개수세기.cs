321644f559eda1176b1c42cfd6ef0837c15ea19e
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-17 22:42:28 +0900

    level2_5_mj_22

---
+++ Level1/쿼드압축후개수세기.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+
+    int[,] temp;
+    int[] answer = new int[] { 0, 0 };
+
+    public int[] solution(int[,] arr)
+    {
+
+        temp = arr;
+
+        int len = arr.GetLength(0);
+
+        DFS(0, 0, len);
+
+        return answer;
+    }
+
+    public void DFS(int x, int y, int len)
+    {
+        bool check_zero = true;
+        bool check_one = true;
+
+        for (int i = x; i < x + len; i++)
+        {
+            for (int j = y; j < y + len; j++)
+            {
+                if (temp[i, j] == 0)
+                {
+                    check_one = false;
+                }
+                if (temp[i, j] == 1)
+                {
+                    check_zero = false;
+                }
+            }
+        }
+        if (check_zero)
+        {
+            answer[0]++;
+            return;
+        }
+        if (check_one)
+        {
+            answer[1]++;
+            return;
+        }
+        DFS(x, y, len / 2);
+        DFS(x, y + len / 2, len / 2);
+        DFS(x + len / 2, y, len / 2);
+        DFS(x + len / 2, y + len / 2, len / 2);
+    }
+}
