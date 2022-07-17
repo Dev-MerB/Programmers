674449c261c63cf80eb158f4300059e53e6567ee
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-17 22:42:08 +0900

    level1_17_mj_21

---
+++ Level1/부족한금액계산하기_mj.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int price, int money, int count)
+{
+    long long answer = -1;
+
+    long long sum = 0;
+    for (int i = 0; i <= count; i++)
+    {
+        sum += price * i;
+    }
+
+    if (sum > money)
+        answer = (sum - money);
+    else
+        answer = 0;
+
+    return answer;
+}
