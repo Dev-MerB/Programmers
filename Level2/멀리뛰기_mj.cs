7f303a6cf6b3271370bfbfd438a7361960ce8bd2
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-19 20:34:21 +0900

    level2_7_mj_24

---
+++ Level2/멀리뛰기_mj.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public long solution(int n)
+    {
+        long answer = 0;
+        int[] temp = new int[n];
+
+        if (n == 1)
+            answer = 1;
+        else
+        {
+            temp[0] = 1;
+            temp[1] = 2;
+
+            for (int i = 2; i < n; i++)
+                temp[i] = (temp[i - 1] % 1234567 + temp[i - 2] % 1234567) % 1234567;
+            answer = temp[n - 1];
+        }
+        return answer;
+    }
+}
