c79b57939209684c94f42698bbb75b8579a67f45
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-22 18:57:52 +0900

    level1_19_mj_27

---
+++ Level1/가운데글자가져오기_mj - 복사본.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Solution
+{
+    public string solution(string s)
+    {
+        string answer = "";
+        char[] charArr = s.ToCharArray();
+        if (s.Length % 2 == 1)
+            answer = charArr[s.Length / 2].ToString();
+        else
+            answer = charArr[s.Length / 2 - 1].ToString() + charArr[s.Length / 2].ToString();
+
+        return answer;
+    }
+}
