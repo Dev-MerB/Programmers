0ac93dd336e432902f51c1220cfeb7ba4caa2519
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-24 23:43:23 +0900

    level1_21_mj_29

---
+++ Level1/나누어떨어지는숫자배열_mj.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public long solution(int a, int b)
+    {
+        long answer = 0;
+        if (a > b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        for (int i = a; i <= b; i++)
+            answer += i;
+        return answer;
+    }
+}
