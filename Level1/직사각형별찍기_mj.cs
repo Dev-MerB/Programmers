0089ae276f016a34c5b537d5fe228c97c0590f74
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-25 00:22:04 +0900

    level1_22_mj_30

---
+++ Level1/직사각형별찍기_mj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+
+        for (int i = 0; i < b; i++)
+        {
+            for (int j = 0; j < a; j++)
+                Console.Write("*");
+            Console.Write("\n");
+        }
+    }
+}
