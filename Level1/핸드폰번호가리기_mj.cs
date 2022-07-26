1a93bcfcf73e678fb5c0a5ea898eae134d86ab67
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-26 15:43:12 +0900

    level1_25_mj_33

---
+++ Level1/핸드폰번호가리기_mj.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public string solution(string phone_number)
+    {
+        string answer = "";
+        for (int i = 0; i < phone_number.Length - 4; i++)
+            answer += "*";
+        for (int i = phone_number.Length - 4; i < phone_number.Length; i++)
+            answer += phone_number[i];
+        return answer;
+    }
+}
