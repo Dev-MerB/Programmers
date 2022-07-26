162817ad1b8c327d32663cb1004982ae735bca96
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-26 15:35:21 +0900

    level1_24_mj_32

---
+++ Level1/행렬의덧셈_mj.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public string solution(int a, int b)
+    {
+        string answer = "";
+        int num = 0;
+        int[] month = { 0, 3, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6 };
+        string[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+
+        num = (month[a - 1] + b - 1) % 7;
+        answer = day[num];
+
+        return answer;
+    }
+}
