29c011322ca3b84dbec0185ef7979577787286ca
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-27 16:56:35 +0900

    level1_26_mj_34

---
+++ Level1/하샤드수_mj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Solution
+{
+    public bool solution(int x)
+    {
+        bool answer = true;
+        int sum = 0;
+        int y = x;
+        while (y > 0)
+        {
+            if (y / 10 == 0 && y % 10 == 0)
+                break;
+            sum += y % 10;
+            y /= 10;
+        }
+        if (x % sum != 0)
+            answer = false;
+        return answer;
+    }
+}
