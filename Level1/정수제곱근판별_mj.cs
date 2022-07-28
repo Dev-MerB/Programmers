6e75988750cf35c27ee28928b15f5d172de50cfc
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-29 01:33:32 +0900

    level1_32_mj_40

---
+++ Level1/정수제곱근판별_mj.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Solution
+{
+    public long solution(long n)
+    {
+        long answer = (long)Math.Sqrt(n);
+        if (n == answer * answer)
+            return (answer + 1) * (answer + 1);
+        else
+            return -1;
+    }
+}
