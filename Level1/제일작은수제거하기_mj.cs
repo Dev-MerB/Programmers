8f5166fc0a9e3604965ee83ecb1f9de115815fd7
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-29 01:32:33 +0900

    level1_31_mj_39

---
+++ Level1/제일작은수제거하기_mj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Solution
+{
+    public int[] solution(int[] arr)
+    {
+        if (arr.Length == 1)
+            return new int[] { -1 };
+        int[] arrSub = arr.Distinct().ToArray();
+        Array.Sort(arrSub);
+        int numToRemove = arrSub[0];
+        int[] answer = arr.Where(val => val != numToRemove).ToArray();
+        return answer;
+    }
+}
