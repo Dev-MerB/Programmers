81507b886ba0b2ea5adf6d32e96130b8e0eb196c
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-28 18:19:23 +0900

    level1_28_mj_36

---
+++ Level1/콜라츠추측_mj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Solution
+{
+    public int solution(int num)
+    {
+        int answer = 0;
+        while (num != 1 && answer != 501)
+        {
+            if (num % 2 == 1)
+            {
+                num *= 3;
+                num += 1;
+            }
+            else
+                num /= 2;
+            answer++;
+        }
+        if (answer > 500)
+            answer = -1;
+        return answer;
+    }
+}
