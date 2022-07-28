e5703649f799cfd9f0d217e522560219a5f55cee
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-29 01:34:43 +0900

    level1_33_mj_41

---
+++ Level1/자연수뒤집어배열로만들기_mj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Solution
+{
+    public int[] solution(long n)
+    {
+        String answerS = n.ToString();
+        int[] answer = new int[answerS.Length];
+        int i = 0;
+        foreach (char s in answerS)
+        {
+            answer[i] = (int)char.GetNumericValue(s);
+            i++;
+        }
+        Array.Reverse(answer);
+        return answer;
+    }
+}
