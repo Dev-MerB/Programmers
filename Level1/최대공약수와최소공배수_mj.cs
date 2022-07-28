7b6fe5bae8de44ed4f0d6ba6bdf3133d356c87a9
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-28 18:20:54 +0900

    level1_29_mj_37

---
+++ Level1/최대공약수와최소공배수_mj.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Solution
+{
+    public int[] solution(int n, int m)
+    {
+        int[] answer = { 1, n * m };
+        for (int i = 1; i <= (n / 2) || i <= (m / 2); i++)
+            if (n % i == 0 && m % i == 0)
+                answer[0] = i;
+        answer[1] /= answer[0];
+        return answer;
+    }
+}
