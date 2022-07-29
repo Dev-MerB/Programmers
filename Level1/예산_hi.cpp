275bcb000669ab79209b77054d3b9d684d2e011f
Jintopia <matrosica0808@gmail.com>
2022-07-29 12:48:03 +0900

    solved 22 problems by hi

---
+++ Level1/예산_hi.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(),d.end());
+    for(auto i: d)
+    {
+        if(budget - i>=0)
+        {
+            budget -=i;
+            answer++;
+        }
+        else
+            break;
+    }
+    return answer;
+}
