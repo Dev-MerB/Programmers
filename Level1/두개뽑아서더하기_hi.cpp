209ac5e72618bc0c7f0052e5d1422a4a408cec34
Jintopia <matrosica0808@gmail.com>
2022-07-29 12:47:55 +0900

    solved 21 problems by hi

---
+++ Level1/두개뽑아서더하기_hi.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for(int i=0; i<numbers.size(); i++)
+        for(int j=i+1; j<numbers.size(); j++)
+            answer.push_back(numbers[i]+numbers[j]);
+    
+    sort(answer.begin(),answer.end());
+    answer.erase(unique(answer.begin(),answer.end()),answer.end());\
+    // unique reference : https://dpdpwl.tistory.com/39
+    return answer;
+}
