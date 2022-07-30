eef61d5eb36cf0da021232db46373e8131ccd86a
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-30 23:41:07 +0900

    level1_34_mj_42

---
+++ Level1/자릿수더하기_mj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        while (n > 0)
+        {
+            answer += n % 10;
+            n /= 10;
+        }
+        answer += n;
+        return answer;
+    }
+}
