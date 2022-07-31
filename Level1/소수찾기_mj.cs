32f6a20033c349c55a83a3325396728f0da7d04a
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-07-31 23:21:23 +0900

    level1_37_mj_45

---
+++ Level1/소수찾기_mj.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            bool count = true;
+            for (int j = 2; j <= (int)Math.Sqrt(n); j++)
+                if (i / j > 1 && i % j == 0)
+                {
+                    count = false;
+                    break;
+                }
+            if (count)
+                answer++;
+        }
+        return answer;
+    }
+}
