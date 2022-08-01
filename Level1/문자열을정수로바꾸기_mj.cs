32536fffe111f86f03a6320d792f600dc052cbbb
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-01 15:05:51 +0900

    level1_38_mj_46

---
+++ Level1/문자열을정수로바꾸기_mj.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Solution
+{
+    public int solution(string s)
+    {
+        int answer = 0;
+        if (s[0] == '-')
+        {
+            s = s.Substring(1, s.Length - 1);
+            answer -= Convert.ToInt32(s);
+        }
+        else
+            answer += Convert.ToInt32(s);
+
+        return answer;
+    }
+}
