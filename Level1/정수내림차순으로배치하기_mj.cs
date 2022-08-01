5ff0d77803ba9ee11a4e347c46252036bbca54f4
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-01 15:06:41 +0900

    level1_39_mj_47

---
+++ Level1/정수내림차순으로배치하기_mj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Solution
+{
+    public long solution(long n)
+    {
+        long answer = 0;
+        char[] c = n.ToString().ToCharArray();
+        Array.Sort(c);
+        Array.Reverse(c);
+        answer = Convert.ToInt64(new string(c));
+        return answer;
+    }
+}
