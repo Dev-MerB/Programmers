618266e88241176309b30d465f2a99148ff4b0d9
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-02 14:34:05 +0900

    level1_40_mj_48

---
+++ Level1/서울에서김서방찾기_mj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Solution
+{
+    public string solution(string[] seoul)
+    {
+        string answer = "";
+        int idx = Array.IndexOf(seoul, "Kim");
+        answer = "김서방은 " + idx + "에 있다";
+        return answer;
+    }
+}
