0c94deb171eb350a31a71afe313a4d44a7dee7b3
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-02 15:15:55 +0900

    level1_42_mj_50

---
+++ Level1/문자열내림차순으로배치하기_mj.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public string solution(string s)
+    {
+        char[] a = s.ToCharArray();
+        char temp = ' ';
+        for (int j = 0; j < a.Length - 1; j++)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] < a[i + 1])
+                {
+                    temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                }
+            }
+        }
+        string answer = new string(a);
+        return answer;
+    }
+}
