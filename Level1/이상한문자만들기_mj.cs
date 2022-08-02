460736c9858267869481d00503b750565ff876ed
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-02 15:07:02 +0900

    level1_41_mj_49

---
+++ Level1/이상한문자만들기_mj.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public string solution(string s)
+    {
+        string answer = "";
+        bool flag = true;
+        char cN = ' ';
+        foreach (char c in s)
+        {
+            cN = c;
+            if (c == 32)
+                flag = false;
+            if (!flag)
+            {
+                if (c < 95 && c > 64)
+                    cN = (char)((int)c + 32);
+                answer += cN;
+                flag = true;
+            }
+            else
+            {
+                if (c > 95)
+                    cN = (char)((int)c - 32);
+                answer += cN;
+                flag = false;
+            }
+        }
+        return answer;
+    }
+}
