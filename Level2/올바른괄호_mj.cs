639e31b1312a5f00a45b3dbedde75afa0efca3bc
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-03 18:54:58 +0900

    level2_9_mj_52

--- Level2/올바른괄호_mj.cs
+++ Level2/올바른괄호_mj.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-
-public class Solution
-{
-    public bool solution(string s)
-    {
+public class Solution{
+    public bool solution(string s){
         int count = 0;
         char[] cA = s.ToCharArray();
         if (cA[0] == 41 || cA[cA.Length - 1] == 40)
             return false;
-        else
-        {
-            foreach (char c in cA)
-            {
+        else{
+            foreach (char c in cA){
                 if (c == 40)
                     count++;
                 else if (c == 41)
