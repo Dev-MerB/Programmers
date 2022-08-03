3fb830da2c2ca2278a057df774ca2a7d9fb1056b
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-03 15:39:12 +0900

    level1_43_mj_51

---
+++ Level1/시저암호_mj.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public string solution(string s, int n)
+    {
+        string answer = "";
+        char cN = ' ';
+        foreach (char c in s)
+        {
+            cN = c;
+            if (cN < 91 && cN > 64)
+            {
+                cN = (char)(cN + n);
+                if (cN > 90)
+                    cN = (char)(cN - 26);
+            }
+            else if (cN > 96 && cN < 123)
+            {
+                cN = (char)(cN + n);
+                if (cN > 122)
+                    cN = (char)(cN - 26);
+            }
+            answer += cN;
+        }
+        return answer;
+    }
+}
