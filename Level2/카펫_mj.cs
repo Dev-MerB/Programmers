641a00b2124c434596435d1b16bd61cd0c0b5ff8
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-06 17:24:45 +0900

    level2_10_mj_53

---
+++ Level2/카펫_mj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(int brown, int yellow)
+    {
+        int[] answer = new int[2];
+        int total = brown + yellow;
+        for (int i = 3; i <= Math.Sqrt(total); i++)
+        {
+            if (total % i == 0 && (i - 2) * (total / i - 2) == yellow)
+            {
+                answer[0] = total / i;
+                answer[1] = i;
+            }
+        }
+        return answer;
+    }
+}
