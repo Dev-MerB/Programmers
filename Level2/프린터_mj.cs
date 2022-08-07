2bb2f78a6e647a5cb8e21cde5b3f6daeaa686786
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-07 22:18:22 +0900

    level2_11_mj_54

---
+++ Level2/프린터_mj.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int[] priorities, int location)
+    {
+        int answer = 0;
+        int i = 0;
+        int num = 1;
+        int[] answers = new int[priorities.Length];
+        while (num <= priorities.Length)
+        {
+            int count = 0;
+            for (int j = 1; j < priorities.Length; j++)
+            {
+                if (priorities[i % priorities.Length] >= priorities[(i + j) % priorities.Length])
+                    count++;
+                else
+                    break;
+            }
+            if (count == (priorities.Length - 1))
+            {
+                priorities[i % priorities.Length] = 0;
+                answers[i % priorities.Length] = num;
+                num++;
+                Console.WriteLine(num);
+            }
+            i++;
+        }
+        answer = answers[location];
+        return answer;
+    }
+}
