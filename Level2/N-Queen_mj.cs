e6d1c3b1fca89ae321d0646fb5f2bb26bfdde969
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2022-08-08 21:58:26 +0900

    level2_12_mj_55

---
+++ Level2/N-Queen_mj.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Solution
+{
+    int answer = 0;
+    int[] nQueen;
+    public int solution(int n)
+    {
+        nQueen = new int[n];
+        nqueen(0, n);
+        return answer;
+    }
+    public void nqueen(int row, int n)
+    {
+        if (row == n)
+        {
+            answer++;
+            return;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            nQueen[row] = i;
+            if (check(row))
+                nqueen(row + 1, n);
+        }
+    }
+    public bool check(int row)
+    {
+        for (int j = 0; j < row; j++)
+            if (nQueen[row] == nQueen[j] || row - j == Math.Abs(nQueen[row] - nQueen[j]))
+                return false;
+        return true;
+    }
+}
